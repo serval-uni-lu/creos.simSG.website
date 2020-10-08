861ec5204c47ed1238b5a1a358a1935b5dd850c4
Ludovic Mouline <ludovic.mouline@uni.lu>
2020-10-08 09:23:34 +0200

    manage approx with bs rules

--- src/plugin/action/loadApprox/load-approx-msg.d.ts
+++ src/plugin/action/loadApprox/load-approx-msg.d.ts
@@ -1,6 +1,10 @@
 import {Message} from "@/types/ws-message";
 import {GridJson} from "@/types/sg-json.types";
 
+type ApproximationType = "naive" | "bs_rule";
+
 interface LoadApproxMsg extends Message {
-    grid: GridJson
+    grid: GridJson;
+    approximationType?: ApproximationType;
 }
+
--- src/plugin/action/uLoadApprox/ULoadApproxVue.vue
+++ src/plugin/action/uLoadApprox/ULoadApproxVue.vue
@@ -1,10 +1,12 @@
 <template lang="pug">
-  button.btn.btn-primary(v-on:click="approximateLoad") Approximate Uncertain Load
+  div
+    button.btn.btn-primary(v-on:click="approximateLoadNaive") Approximate Uncertain Load (Naive)
+    button.btn.btn-primary(v-on:click="approximateLoadBsRule") Approximate Uncertain Load (with rules)
 </template>
 
 <script lang="ts">
     import {Vue, Component} from "vue-property-decorator";
-    import {LoadApproxMsg} from "@/plugin/action/loadApprox/load-approx-msg";
+    import {ApproximationType, LoadApproxMsg} from "@/plugin/action/loadApprox/load-approx-msg";
     import * as WS from "@/ws";
     import toJson from "@/utils/grid-state-utils";
     import GridState from "@/store/modules/grid-state";
@@ -13,9 +15,18 @@
     @Component
     export default class ULoadApproxVue extends Vue {
 
-      public approximateLoad() {
+      public approximateLoadNaive() {
+        this.approximateLoad("naive")
+      }
+
+      public approximateLoadBsRule() {
+        this.approximateLoad("bs_rule")
+      }
+
+      public approximateLoad(type: ApproximationType) {
         const info: LoadApproxMsg = {
           type: "ULoadApproximation",
+          approximationType: type,
           grid: toJson((this.$store as any)._modulesNamespaceMap["GridState/"].state as GridState)
         }
 
@@ -24,6 +35,9 @@
     }
 </script>
 
-<style scoped>
-
+<style scoped lang="scss">
+  button {
+    margin-bottom: 5px;
+    margin-top: 5px;
+  }
 </style>
